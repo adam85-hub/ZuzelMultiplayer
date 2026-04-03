5613f7d7eed69897d88749f31fda9d742840e6a4
Adam <56732487+adam85-hub@users.noreply.github.com>
2026-04-03 13:44:46 +0200

    Refactored class Player (#2)

--- Player.h
+++ Player.h
@@ -1,6 +1,5 @@
 #pragma once
-#define _USE_MATH_DEFINES
-#include <math.h>
+#include <allegro5/allegro5.h>
 
 #include "GameObject.h"
 #include "vec2.h"
@@ -8,30 +7,10 @@
 class Player : public GameObject {
 public:
 	// z bike_bitmap jest tworzona jej przeskalowana kopia. Po zakoñczeniu wykonywania siê konstruktora bitmapa pod adresem bike_bitmap nie jest ju¿ potrzebna
-	Player(Utils::vec2 initial_position, ALLEGRO_BITMAP* bike_bitmap) : GameObject(initial_position) {
-		constexpr float scale = 2;
-		_bike_bitmap_size.x = al_get_bitmap_width(bike_bitmap) * scale;
-		_bike_bitmap_size.y = al_get_bitmap_height(bike_bitmap) * scale;
-
-		_bike_bitmap = al_create_bitmap(_bike_bitmap_size.x, _bike_bitmap_size.y);
-		al_set_target_bitmap(_bike_bitmap);
-		al_draw_scaled_bitmap(bike_bitmap, 0, 0, al_get_bitmap_width(bike_bitmap), al_get_bitmap_height(bike_bitmap),
-			0, 0, _bike_bitmap_size.x, _bike_bitmap_size.y, 0);
-		al_convert_mask_to_alpha(_bike_bitmap, al_map_rgb(0, 0, 0));
-		al_set_target_backbuffer(al_get_current_display());
-	};
-
-	~Player() {
-		al_destroy_bitmap(_bike_bitmap);
-	}
-
-	void Update() {
-
-	}
-
-	void Render() {
-		al_draw_rotated_bitmap(_bike_bitmap, _bike_bitmap_size.x/2, _bike_bitmap_size.y/2, position.x, position.y, 0, 0);
-	}
+	Player(Utils::vec2 initial_position, ALLEGRO_BITMAP* bike_bitmap);
+	~Player();
+	void Update();
+	void Render();
 
 private:
 	ALLEGRO_BITMAP* _bike_bitmap;
