ef7d2240a60c86317b32191ce33b3a984f62c84c
adam85-hub <adam.bialik85@gmail.com>
2026-04-08 14:21:42 +0200

    Added: Pressing "R" during the race restarts it

--- SC_R_Update.cpp
+++ SC_R_Update.cpp
@@ -2,6 +2,11 @@
 #include "Consts.h"
 
 void RaceScene::Update(KeyStatesTable key_states) {
+	// restart wyœcigu:
+	if (key_states[ALLEGRO_KEY_R] & c_KEY_PRESSED) {
+		_game_commands->switch_scene.Execute(new RaceScene(_game_commands, _number_of_players));
+	}
+
 	for (int i = 0; i < _number_of_players; i++) {
 		bool is_turning = key_states[_turn_buttons[i]] & c_KEY_DOWN;
 		_players[i]->Update(is_turning);
