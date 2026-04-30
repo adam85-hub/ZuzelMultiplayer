b644b58cdf039ca337ddeae4681e4cffacff0cbe
adam85-hub <adam.bialik85@gmail.com>
2026-04-30 12:21:42 +0200

    Naprawiłem działanie operatorów dwuargumentowych na vec2

--- vec2.h
+++ vec2.h
@@ -24,32 +24,20 @@ namespace Utils {
 			return sqrt(powf(this->x - x, 2) + powf(this->y - y, 2));
     }
 
-		vec2& operator+(const vec2& other) {
-			x += other.x;
-			y += other.y;
-
-			return *this;
+		vec2 operator+(const vec2& other) const {
+			return vec2(x + other.x, y+other.y);
 		}
 
-
-		vec2& operator-(const vec2& other) {
-			x -= other.x;
-			y -= other.y;
-
-			return *this;
+		vec2 operator-(const vec2& other) const {
+			return vec2(x - other.x, y - other.y);
 		}
 
-
-		vec2& operator*(float scalar) {
-			x *= scalar;
-			y *= scalar;
-
-			return *this;
+		vec2 operator*(float scalar) const {
+			return vec2(x * scalar, y * scalar);
 		}
 
 		float dot(const vec2& other) const {
 			return (x * other.x) + (y * other.y);
 		}
-
 	};
 }
