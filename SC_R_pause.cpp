6f43a689c6edc57875fb306b05a7f8beeb8bb3b9
adam85-hub <adam.bialik85@gmail.com>
2026-05-23 13:13:25 +0200

    Pauza działa teraz też na timer

--- SC_R_pause.cpp
+++ SC_R_pause.cpp
@@ -2,12 +2,14 @@
 
 void RaceScene::pause() {
 	_paused = true;
-	if(!_start_countdown.Has_ended())
+	if (!_start_countdown.Has_ended())
 		_start_countdown.Pause();
+	_race_timer.Pause(); 
 }
 
 void RaceScene::unpause() {
 	_paused = false;
 	if (!_start_countdown.Has_ended())
 		_start_countdown.Unpause();
+	_race_timer.Unpause();
 }
