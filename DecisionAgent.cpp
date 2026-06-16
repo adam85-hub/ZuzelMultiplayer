8d68c31062b6b250dfb0d900b6b6b9a4132b5d5b
Zimorodek PL <henryk.kazmierczak2006@gmail.com>
2026-06-16 17:40:58 +0200

    changed fnn loading

--- DecisionAgent.cpp
+++ DecisionAgent.cpp
@@ -1,7 +1,15 @@
 #include "DecisionAgent.hpp"
+#include "DQNAManager.h"
 
 DecisionAgent::DecisionAgent(FNN fnn, size_t actionsCount, double epsilon): actionsCount(actionsCount), epsilon(epsilon) {
-	this->neuralNetwork = fnn;
+	this->neuralNetwork = FNN();
+	this->neuralNetwork.addInputLayer(PARAM_COUNT * STATES_TO_PROCESS);
+	this->neuralNetwork.addLayer(HIDDEN_LAYERS_SIZE);
+	this->neuralNetwork.addLayer(HIDDEN_LAYERS_SIZE);
+	this->neuralNetwork.addOutputLayer(ACTION_COUNT);
+	this->neuralNetwork.setActivationFunction(AFunc::L_RELU);
+	this->neuralNetwork.setOutputLayerActivationFunction(AFunc::LINEAR);
+	this->neuralNetwork.cloneWeights(fnn);
 }
 
 size_t DecisionAgent::act(const std::vector<double>& state, bool getConsoleOutput) {
