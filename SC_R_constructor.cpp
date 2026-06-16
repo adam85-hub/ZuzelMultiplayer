bf63d83026d910646b1cb223a348b21837e344d9
adam85-hub <adam.bialik85@gmail.com>
2026-06-16 19:25:05 +0200

    Dodanie z powrotem odliczania do startu

--- SC_R_constructor.cpp
+++ SC_R_constructor.cpp
@@ -14,7 +14,7 @@ RaceScene::RaceScene(GameCommands* gameCommands, short number_of_players, short 
 	_number_of_ais(number_of_ais),
 	_total_player_count(number_of_players + number_of_ais),
 	_turn_buttons{ ALLEGRO_KEY_LCTRL, ALLEGRO_KEY_SPACE, ALLEGRO_KEY_RCTRL, ALLEGRO_KEY_DOWN },
-	_start_countdown(0),
+	_start_countdown(3),
 	_decision_agent(NNFileManager::loadFNN(c_FNN_PATH), 2, 0.1)
 {
 	// inicjalizacja wszystkich polygonów
